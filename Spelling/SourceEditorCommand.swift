53403491a454763e8e27cf3955c52d5d1729adfa
Victor Mitelea <victor.mitelea@3pillarglobal.com>
2018-03-30 14:47:03 +0300

    - added line selection if find some wrong spelling

--- Spelling/SourceEditorCommand.swift
+++ Spelling/SourceEditorCommand.swift
@@ -13,8 +13,7 @@ import AppKit
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     let spellChecker = NSSpellChecker.shared
 
-    let supportUTIs = [
-        "public.swift-source"]
+    let supportUTIs = ["public.swift-source"]
 
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) {
         // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
@@ -30,17 +29,21 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
 
         for line in invocation.buffer.lines.lazy.enumerated() {
             guard let stringLine = line.element as? String else {
-                break
+                continue
             }
 
             let checkRange = spellChecker.checkSpelling(of: stringLine, startingAt: stringLine.startIndex.encodedOffset)
 
             if let word = stringLine.substring(with: checkRange) {
+                let lineSelection = XCSourceTextRange()
+                lineSelection.start = XCSourceTextPosition(line: line.offset, column: 0)
+                lineSelection.end = XCSourceTextPosition(line: line.offset + 1, column: 0)
+                invocation.buffer.selections.add(lineSelection)
+
                 invocation.buffer.lines.add("Word \(word) is incorrect in line \(line.offset + 1)")
             }
         }
 
         completionHandler(nil)
     }
-
 }
