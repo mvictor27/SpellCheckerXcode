58fd221411d57c0e42c14698325253c39eb7c212
Victor Mitelea <victor.mitelea@3pillarglobal.com>
2018-03-30 16:55:51 +0300

    -  added functionality for selected lines

--- Spelling/SourceEditorCommand.swift
+++ Spelling/SourceEditorCommand.swift
@@ -15,35 +15,73 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
 
     let supportUTIs = ["public.swift-source"]
 
-    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) {
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void) {
         // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        defer {
+            completionHandler(nil)
+        }
 
         let uti = invocation.buffer.contentUTI
 
         guard supportUTIs.contains(uti) else {
-            completionHandler(nil)
             return
         }
 
+        //TODO: Move this methd
         spellChecker.learnWord("func")
+
+        if let selectedLines = invocation.buffer.selections.firstObject as? XCSourceTextRange, selectedLines.start.line != selectedLines.end.line {
+
+            extractSpelling(fromSelectedLines: selectedLines, invocation)
+            return
+        }
+
+        extractSpellingFromPage(invocation)
+    }
+
+}
+
+// MARK: Private Methods
+extension SourceEditorCommand {
+
+    private func extractSpellingSubstring(from text: String) -> Substring? {
+        let checkRange = spellChecker.checkSpelling(of: text, startingAt: text.startIndex.encodedOffset)
+        return text.substring(with: checkRange)
+    }
+
+    private func selectLine(at offset: Int) -> XCSourceTextRange {
+        let textRange = XCSourceTextRange()
+        textRange.start = XCSourceTextPosition(line: offset, column: 0)
+        textRange.end = XCSourceTextPosition(line: offset + 1, column: 0)
+        return textRange
+    }
+
+    private func extractSpellingFromPage(_ invocation: XCSourceEditorCommandInvocation) {
 
         for line in invocation.buffer.lines.lazy.enumerated() {
             guard let stringLine = line.element as? String else {
                 continue
             }
 
-            let checkRange = spellChecker.checkSpelling(of: stringLine, startingAt: stringLine.startIndex.encodedOffset)
-
-            if let word = stringLine.substring(with: checkRange) {
-                let lineSelection = XCSourceTextRange()
-                lineSelection.start = XCSourceTextPosition(line: line.offset, column: 0)
-                lineSelection.end = XCSourceTextPosition(line: line.offset + 1, column: 0)
-                invocation.buffer.selections.add(lineSelection)
-
+            if let word = extractSpellingSubstring(from: stringLine) {
+                let selectedLine = selectLine(at: line.offset)
+                invocation.buffer.selections.add(selectedLine)
                 invocation.buffer.lines.add("Word \(word) is incorrect in line \(line.offset + 1)")
             }
         }
-
-        completionHandler(nil)
     }
+
+    private func extractSpelling(fromSelectedLines selectedLines: XCSourceTextRange, _ invocation: XCSourceEditorCommandInvocation) {
+
+        for lineIndex in selectedLines.start.line ... selectedLines.end.line {
+            guard let stringLine = invocation.buffer.lines[lineIndex] as? String else {
+                continue
+            }
+
+            if let word = extractSpellingSubstring(from: stringLine) {
+                invocation.buffer.lines.add("Word \(word) is incorrect in line \(lineIndex + 1)")
+            }
+        }
+    }
+
 }
